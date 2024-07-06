83cbf1fbdd9bfc56b80a3d2030ef94dfc1ec8b00
Vadym Volin <vadim.volin1@gmail.com>
2024-07-06 19:59:52 +0300

    M-10: [BE] Sign in - Sign in with social networks - added google

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     // firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.play.services.auth)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
